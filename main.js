70cfd59ca29352f870ba3f02a81442611283d79a
rajatjain15 <79210039+rajatjain15@users.noreply.github.com>
2022-02-06 14:50:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+RightwristX=0;
+LeftwristX=0;
+difference=0;
+
+function setup()
+{
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet Is Initalized...");
+}
+
+function gotPoses(results)
+{
+if(results.length>0)
+{
+    console.log(results);
+}
+}
